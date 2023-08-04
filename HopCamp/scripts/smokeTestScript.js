dedd5770fdca912c379cd9f98104323cf370634a
DanielPurdy509 <danielpurdy509@gmail.com>
2023-08-04 12:08:13 -0700

    Adding local code to repository

--- HopCamp/scripts/smokeTestScript.js
+++ HopCamp/scripts/smokeTestScript.js
@@ -6,6 +6,8 @@ export const options = {
   duration: '1m', // This can be shorter or just a few iterations
 };
 
+//Joe's Droplet
+
 export default () => {
   let res1 = http.get('http://159.223.122.220:5001/api/campsites');
   let res2 = http.get('http://159.223.122.220:5001/api/ratings');
@@ -26,3 +28,26 @@ export default () => {
   // Step2
   // etc.
 };
+
+// Israr's Droplet
+
+// export default () => {
+//     let res1 = http.get('http://134.122.22.60:5001/api/campsites');
+//     let res2 = http.get('http://134.122.22.60:5001/api/ratings');
+//     let res3 = http.get('http://134.122.22.60:5001/api/camping-spots');
+//     let res4 = http.get('http://134.122.22.60:5001/api/campers-also');
+//     let res5 = http.get('http://134.122.22.60:5001/api/photogallery');
+//     let res6 = http.get('http://134.122.22.60:5001/api/things-nearby');
+//     check(res1, { 'status was 200': r => r.status == 200 });
+//     check(res2, { 'status was 200': r => r.status == 200 });
+//     check(res3, { 'status was 200': r => r.status == 200 });
+//     check(res4, { 'status was 200': r => r.status == 200 });
+//     check(res5, { 'status was 200': r => r.status == 200 });
+//     check(res6, { 'status was 200': r => r.status == 200 });
+//     sleep(1);
+//     // MORE STEPS
+//     // Here you can have more steps or complex script
+//     // Step1
+//     // Step2
+//     // etc.
+//   };
